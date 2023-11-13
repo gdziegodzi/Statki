1f03baa81c0bc3c1d2ece79a019e9fcbcc450d55
Filip <131429926+Filip-Ku@users.noreply.github.com>
2023-11-13 20:32:34 +0100

    Add files via upload

--- main_menu.py
+++ main_menu.py
@@ -50,14 +50,19 @@ def draw_main_menu():
     total_button_height = (button_height + button_spacing) * len(menu_buttons)
     button_y = (SCREEN_HEIGHT - total_button_height) // 2
 
-    for button in menu_buttons:
+    for i, button in enumerate(menu_buttons):
         button_text = font.render(button["text"], 1, button_text_color)
         button_width = 300
         button_x = (SCREEN_WIDTH - button_width) // 2
 
-
         button_rect = pygame.Rect(button_x, button_y, button_width, button_height)
-        pygame.draw.rect(screen, button_color, button_rect)
+
+        # Check if the mouse is over the button
+        if button_rect.collidepoint(pygame.mouse.get_pos()):
+            pygame.draw.rect(screen,(0, 0, 0), pygame.Rect(button_x-5,button_y-5,button_width+10, button_height+10))
+            pygame.draw.rect(screen, (100, 50, 50), button_rect)
+        else:
+            pygame.draw.rect(screen, button_color, button_rect)
 
         text_x = button_rect.centerx - button_text.get_width() // 2
         text_y = button_rect.centery - button_text.get_height() // 2
@@ -67,6 +72,7 @@ def draw_main_menu():
         button_y += button_height + button_spacing
 
     pygame.display.update()
+
 
 # Function to start a new game
 def start_game():
@@ -106,11 +112,13 @@ clock = pygame.time.Clock()
 
 while run:
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+
             if event.button == 1:  # Left mouse button
                 for i, button in enumerate(menu_buttons):
                     button_width = font.render(button["text"], 1, button_text_color).get_width() + 20
